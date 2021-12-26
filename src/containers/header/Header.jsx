93b27cadfca4180c980b5e8dc28cb3ea8afb4108
maidul.bappy <maidul.bappy.001@gmail.com>
2021-12-27 01:14:49 +0600

    header content markup

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import ai from "../../assets/ai.png";
+import people from "../../assets/people.png";
 import "./header.css";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="gp3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let’s Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+        <div className="gpt3__header-content__input">
+          <input type="email" name="" id="" placeholder="Your Email Address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+        <div className="gpt3__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
     </div>
   );
 };
