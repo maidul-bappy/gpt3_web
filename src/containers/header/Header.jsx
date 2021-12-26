18f065dc91afa5935e843d4d0f071268212997c6
maidul.bappy <maidul.bappy.001@gmail.com>
2021-12-27 04:50:48 +0600

    header design finished

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -5,7 +5,7 @@ import "./header.css";
 
 const Header = () => {
   return (
-    <div className="gp3__header section__padding" id="home">
+    <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
@@ -24,9 +24,9 @@ const Header = () => {
           <img src={people} alt="people" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className="gpt3__header-image">
-          <img src={ai} alt="ai" />
-        </div>
+      </div>
+      <div className="gpt3__header-image">
+        <img src={ai} alt="ai" />
       </div>
     </div>
   );
